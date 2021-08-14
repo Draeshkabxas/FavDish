791b0a77e008b3fb64af9080b32f721c2f6bf1b2
I-TEECK <zzxxccz908@gmail.com>
2021-08-14 10:49:48 +0200

    Update: add animation to tv_app_name in activity_splash.xml

--- app/src/main/java/com/derar/libya/favdish/SplashActivity.kt
+++ app/src/main/java/com/derar/libya/favdish/SplashActivity.kt
@@ -1,33 +1,59 @@
 package com.derar.libya.favdish
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
 import com.derar.libya.favdish.databinding.ActivitySplashBinding
+import android.os.Handler
+
+
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val splashBinding : ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)
 
-        
-       setFullScreen()
-        
-        
+        //Make activity full screen
+        setFullScreen()
+
+        /**
+         * Set tv_app_name animation
+         */
+        val splashAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_splash)
+        splashBinding.tvAppName.animation = splashAnimation
+
+        /**
+         * Add animation listener for go to main activity when the animation is end
+         */
+        splashAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {}
+            override fun onAnimationEnd(p0: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                  startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+                    finish()
+                },1000)
+            }
+            override fun onAnimationRepeat(p0: Animation?) {}
+        })
+
     }
 
     /**
      * This function wet the layout ot be full screen
      */
     private fun setFullScreen() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
-        }else{
+        } else {
             @Suppress("DEPRECATION")
             window.setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
