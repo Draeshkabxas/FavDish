fc7642131ead4dba224859b4eb612736746521d3
I-TEECK <zzxxccz908@gmail.com>
2021-08-15 15:18:20 +0200

    Update: add logic to save the image that come from the gallery or camera for use it again.

--- app/src/main/java/com/derar/libya/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/derar/libya/favdish/view/activities/AddUpdateDishActivity.kt
@@ -4,7 +4,11 @@ import android.Manifest
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -16,9 +20,17 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.Nullable
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.derar.libya.favdish.R
 import com.derar.libya.favdish.databinding.ActivityAddUpdateDishBinding
 import com.derar.libya.favdish.databinding.DialogCustomImageSelectionBinding
@@ -30,10 +42,17 @@ import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.karumi.dexter.listener.single.PermissionListener
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.util.*
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mBinding: ActivityAddUpdateDishBinding
+
+    private var mImagePath = ""
 
     private lateinit var getImageFromGallery: ActivityResultLauncher<String?>
     private lateinit var getPhotoFromCamera: ActivityResultLauncher<Void?>
@@ -56,25 +75,32 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
         mBinding.ivAddDishImage.setOnClickListener(this@AddUpdateDishActivity)
     }
-
+    /**
+     * initialize getPhotoFromCamera for take photo from camera
+     */
     @RequiresApi(Build.VERSION_CODES.P)
     private fun initializeGetPhotoFromCamera() {
         getPhotoFromCamera = registerForActivityResult(
             ActivityResultContracts.TakePicturePreview(),
             ActivityResultCallback {
-              mBinding.ivDishImage.setImageBitmap(it)
+              setDishImage(it)
              changeAddDishImageToEditImage()
             }
         )
     }
 
+    /**
+     * Change add_dish image to be edit_dish image
+     */
     private fun changeAddDishImageToEditImage() {
         mBinding.ivAddDishImage.setImageDrawable(
             ContextCompat.getDrawable(this@AddUpdateDishActivity,R.drawable.ic_vector_edit)
         )
     }
 
-
+    /**
+     * Lunch getPhotoFromCamera for take photo from camera
+     */
     private fun lunchGetPhotoFromCamera() {
         try {
           getPhotoFromCamera.launch(null)
@@ -87,20 +113,69 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
             ).show()
         }
     }
-
+    /**
+     * initialize GetImageFromGallery for get image from the gallery
+     */
     @RequiresApi(Build.VERSION_CODES.P)
     private fun initializeGetImageFromGallery() {
         getImageFromGallery = registerForActivityResult(
             ActivityResultContracts.GetContent(),
             ActivityResultCallback {
-                mBinding.ivDishImage.setImageURI(it)
+
+                //Get dish image from gallery and make id DishImage
+               setDishImage(it)
+
+                //Set add_dish to be edit_dish
                 changeAddDishImageToEditImage()
             }
         )
     }
 
     /**
-     *
+     * Set imageDish to be passed image
+     */
+    private fun setDishImage(it: Any?) {
+        // Set Selected Image URI to the imageView using Glide
+        Glide.with(this@AddUpdateDishActivity)
+            .load(it)
+            .centerCrop()
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    @Nullable e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    // log exception
+                    Log.e("TAG", "Error loading image", e)
+                    return false // important to return false so the error placeholder can be placed
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+
+                    // TODO Step 6: Get the Bitmap and save it to the local storage and get the Image Path.
+                    val bitmap: Bitmap = resource.toBitmap()
+
+                    mImagePath = bitmap.saveImageToInternalStorage()
+                    Log.i("ImagePath", mImagePath)
+                    return false
+                }
+            })
+            .into(mBinding.ivDishImage)
+
+
+    }
+
+
+    /**
+     * Lunch getImageFromGallery for go to the gallery and get an image
      */
     private fun lunchGetImageFromGallery() {
         try {
@@ -258,4 +333,53 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
             }.show()
     }
     // END
+
+    /**
+     * A function to save a copy of an image to internal storage for FavDishApp to use.
+     *
+     * @param bitmap
+     */
+    private fun Bitmap.saveImageToInternalStorage():String {
+        // Get the context wrapper instance
+        val wrapper = ContextWrapper(applicationContext)
+
+        // Initializing a new file
+        // The bellow line return a directory in internal storage
+        /**
+         * The Mode Private here is
+         * File creation mode: the default mode, where the created file can only
+         * be accessed by the calling application (or all applications sharing the
+         * same user ID).
+         */
+        var file = wrapper.getDir(IMAGE_DIRECTORY, Context.MODE_PRIVATE)
+
+        // Mention a file name to save the image
+        file = File(file, "${UUID.randomUUID()}.jpg")
+
+        try {
+            // Get the file output stream
+            val stream: OutputStream = FileOutputStream(file)
+
+            // Compress bitmap
+            this.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+
+            // Flush the stream
+            stream.flush()
+
+            // Close stream
+            stream.close()
+        } catch (e: IOException) { // Catch the exception
+            e.printStackTrace()
+        }
+
+        // Return the saved image absolute path
+        return file.absolutePath
+    }
+
+
+
+    companion object{
+        // Declare a constant variable for directory name to store the images.
+        const val IMAGE_DIRECTORY="FavDishImages"
+    }
 }
